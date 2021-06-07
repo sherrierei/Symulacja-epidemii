a64c8e2c1342aec4b6d7b4c427e7c3c7b829b4cb
Szymon Malec <77552397+Szymex49@users.noreply.github.com>
2021-06-07 10:46:18 +0200

    Przebudowa symulacji
    - wprowadzenie lockdownów i szczepień

--- Symulacja.jl
+++ Symulacja.jl
@@ -1,60 +1,144 @@
 using Plots
 
-
-function area_epidemic_simulation(size, N0, meetings, infection_progress, loosing_resist_prop, lockdowns,
-    vaccine_day, vaccinated, death_prop, recovery_prop, infection_prop, T)
+function area_epidemic_simulation(size, N0, meetings, incubation_time, immunity_time, lockdowns,
+    vaccination_day, vaccinations, death_prop, recovery_prop, infection_propability, T)
     # 0 - dead
     # 1 - susceptible
     # 2 - exposed
     # 3 - infected
     # 4 - recovered
 
-    deaths = 0
+    dead = []
+    susceptible = collect(1:size^2)
+    exposed = []
+    infected = []
+    recovered = []
+    vaccinated = []
+    infected_record = [N0]
     population = ones(size, size)
+    infection_days = fill(T, size, size)
+    recovery_days = fill(T, size, size)
+    vaccination_days = fill(T, size, size)
+
+    # Random first infected
     for person in 1:N0
-        population[rand(1:size), rand(1:size)] = 3
+        index = rand(susceptible)
+        population[index] = 3
+        append!(infected, index)
+        filter!(i -> i != index, susceptible)
     end
 
+    # For each day in T days
     anim = @animate for t in 1:T
+        infection_prop = infection_propability
+        met_today = meetings
+        
+        # If it's lockdown
+        for lockdown in lockdowns
+            if lockdown[1] <= t <= lockdown[2]
+                infection_prop = infection_prop/2
+                met_today = 1
+            end
+        end
+
+        # Vaccination
+        if t >= vaccination_day
+            for person in 1:vaccinations
+                try
+                    index = rand(vcat(susceptible, exposed, recovered))
+                    append!(vaccinated, index)
+                    filter!(i -> i != index, susceptible)
+                    filter!(i -> i != index, exposed)
+                    filter!(i -> i != index, recovered)
+                catch ArgumentError
+                    continue
+                end
+            end
+        end
+
+        # For each person
         for row in 1:size
             for person in 1:size
+
+                index = size*(person-1) + row
+                
+                # If person is exposed and incubation time has ended, change his status to infected
+                if population[index] == 2 && t - infection_days[index] >= incubation_time
+                    population[index] = 3
+                    append!(infected, index)
+                    filter!(i -> i != index, exposed)
+                end
+                
+                # If the person is infected
                 if population[row, person] == 3
                     
-                    for met_person in 1:meetings
+                    # For each met person from the neighbourhood
+                    for met_person in 1:met_today
                         if rand()<infection_prop
                             m = rand(-1:1)
                             n = rand(-1:1)
+                            new_infected_index = size * (person + n - 1) + row + m
                             try
-                                if population[row + m, person + n] == 1
-                                    population[row + m, person + n] = 3
+                                if population[new_infected_index] == 1 && new_infected_index ∉ vaccinated
+                                    population[new_infected_index] = 2
+                                    append!(exposed, new_infected_index)
+                                    filter!(i -> i != new_infected_index, susceptible)
+                                    infection_days[new_infected_index] = t
                                 end
-                            catch e
+                            catch BoundsError
                                 continue
                             end
                         end
                     end
                     
                     if rand()<recovery_prop
-                        population[row, person] = 4
+                        population[index] = 4
+                        append!(recovered, index)
+                        filter!(i -> i != index, infected)
+                        recovery_days[row, person] = t
+
                     elseif rand()<death_prop
-                        population[row, person] = 0
-                        deaths += 1
+                        population[index] = 0
+                        append!(dead, index)
+                        filter!(i -> i != index, infected)
                     end
-                    
-                elseif population[row, person] == 4
-                    if rand()<loosing_resist_prop
-                        population[row, person] = 1
-                    end
+                
+                # If person looses immunity
+                elseif population[index] == 4 && t - recovery_days[index] >= immunity_time
+                    population[index] = 1
+                    append!(susceptible, index)
+                    filter!(i -> i != index, recovered)
                 end
             end
         end
+        append!(infected_record, length(infected))
+        deaths = length(dead)
+        
         heatmap(population,
             title="Day: $t    Deaths: $deaths",
             color=[:black, :gray, :yellow, :orange, :green],
             clim=(0, 4),
-            size=(700, 700),
+            size=(650, 650),
             aspectratio=1
         )
     end
+    plot(0:T, infected_record) |> display
     gif(anim, "epidemic.gif", fps=15) |> display
 end
+
+
+size = 100
+N0 = 10
+meetings = 3
+incubation_time = 3
+immunity_time = 90
+lockdowns = []
+vaccination_day = 400
+vaccinations = 100
+death_prop = 0.001
+recovery_prop = 0.05
+infection_propability = 0.2
+T = 800
+
+area_epidemic_simulation(size, N0, meetings, incubation_time, immunity_time, lockdowns,
+        vaccination_day, vaccinations, death_prop, recovery_prop, infection_propability, T)
